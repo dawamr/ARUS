f09a6bcdd78b6e8911c5b05b8204e7a6ddec48e6
Dawam Raja <akunnyadawam9@gmail.com>
2019-11-28 13:22:11 +0700

    add cors q q

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const bodyParser = require("body-parser");
 const apiEndpoint = require("./src/routes");
-// const cors = require("cors");
+const cors = require("cors");
 // Set up the express app
 const app = express();
 
